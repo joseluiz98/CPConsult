505217280fc7a8b0f75059689f8916a5d9d28586
José Luiz do Carmo Silva <joseluiz98.jl@gmail.com>
2018-01-11 22:16:12 -0200

    Seções configuradas

--- content/js/js-adicional.js
+++ content/js/js-adicional.js
@@ -1,5 +1,29 @@
-$(document).ready(function(){
-    /*$(".dropdown-item").click(function(){
-        $(this).css("background-color","green");
-    });*/
+$(document).ready(function(){    
+    // Scripts para scroll down de página quando clica nos botões
+    document.getElementById("company-about-button").addEventListener('click',function ()
+    {
+        scrollToItem("company-about-section");
+    });
+
+    document.getElementById("navbar-company-about").addEventListener('click',function ()
+    {
+        scrollToItem("company-about-section");
+    });
+
+    // Scripts para scroll down de página quando clica nos links da navbar
+    $("nav").find("a").click(function(e) {
+        e.preventDefault();
+        var section = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(section).offset().top
+        });
+    });
+
+    //Funções
+    // Função Scroll down suave para botões
+    function scrollToItem(itemID) {
+        document.getElementById(itemID).scrollIntoView({ 
+            behavior: 'smooth' 
+          });
+    }
 })
