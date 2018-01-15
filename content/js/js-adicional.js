bfd59617ea7b2f37ee19083c802efbdbd372d766
José Luiz do Carmo Silva <joseluiz98.jl@gmail.com>
2018-01-15 01:32:58 -0200

    Bug fixes
    - Cursor do mouse agora muda em todos os links
    - Broken links consertados.

--- content/js/js-adicional.js
+++ content/js/js-adicional.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
+    //Fecha navbar ao clicar em seus links, menos o "A Empresa"
+    $(document).on('click','.navbar-collapse', function(e) {
+        if($(e.target).is('a')) {
+            if(event.target.id != "navbar-company-about"){
+                $(this).collapse('hide');
+            }
+        }
+    });
+    
     //Testa se navbar está collapsed ou normal ao evento click
     $('#company-about-button, #navbar-company-about').click(function () {
         var id = this.id;
-
+        //Se é mobile, fecha o menu após clicar em opção "A Empresa"
         if($('.navbar-toggler').css('display') == 'block'){
             $('html, body').animate({
                 scrollTop: $('#company-about-section').offset().top - 20
